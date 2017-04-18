b3ddcddd8bab02b30323cdcb503f611d929f451f
Brittany Herr <brittanyherr2018@u.northwestern.edu>
2017-04-18 14:58:59 -0500

    adds success page

--- stripe.php
+++ stripe.php
@@ -1,33 +1,19 @@
-<html>
-<head>
-<script src="https://js.stripe.com/v3/"></script>
-</head>
-<body>
-
-
-
-
 <?php 
 
-echo('Hello world!');
+echo("<link rel=\"stylesheet\" href=\"stripe.css\">");
+echo("<link href=\"https://fonts.googleapis.com/css?family=Lato\" rel=\"stylesheet\">");
+echo("<body>");
+echo("<img class=\"logo\" src=\"/img/favicon.png\">");
+echo("<h1>Success!</h1>");
+echo("<p>Your donation is now on its way to Firstname's fund.</p>");
+echo("<p>You can now close this window and continue shopping</p>");
+echo("<h3 class=\"foot\">Powered by Stripe</h3>");
+
+
+
 require_once('vendor/autoload.php');
 \Stripe\Stripe::setApiKey("sk_test_sc7R4O8I5qe2UgBLryNVcr2C"); 
 $token = $_POST['stripeToken'];
-// 	$token = $_POST['stripeToken'];
-
-// echo $_POST["stripeToken"]
-		// Charge the user's card:
-		// ate a Customer:
-// $customer = \Stripe\Customer::create(array(
-//   "email" => "paying.user@example.com",
-//   "source" => $token,
-// ));
-// // Charge the Customer instead of the card:
-// $charge = \Stripe\Charge::create(array(
-//   "amount" => 1000,
-//   "currency" => "usd",
-//   "customer" => $customer->id
-// ));
 
 $charge = \Stripe\Charge::create(array(
   "amount" => 1000,
@@ -38,7 +24,3 @@ $charge = \Stripe\Charge::create(array(
 
 ?>
 
-
-
-</body>
-</html>
