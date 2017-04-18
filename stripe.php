aacfbf8568c365b6141446b4cd5edc5cc6506bfb
Kitty Liu <kitty.r.liu@gmail.com>
2017-04-17 23:52:49 -0500

    credit card transaction form works

--- stripe.php
+++ stripe.php
@@ -1,23 +1,43 @@
 <html>
+<head>
+<script src="https://js.stripe.com/v3/"></script>
+</head>
 <body>
 
 
-Your token is <?php echo $_GET["token"]; ?>
+
 
 <?php 
-	$token = $_GET["token"];
 
-	\Stripe\Stripe::setApiKey("sk_test_sc7R4O8I5qe2UgBLryNVcr2C"); 
+echo('Hello world!');
+require_once('vendor/autoload.php');
+\Stripe\Stripe::setApiKey("sk_test_sc7R4O8I5qe2UgBLryNVcr2C"); 
+$token = $_POST['stripeToken'];
+// 	$token = $_POST['stripeToken'];
 
-	$charge = \Stripe\Charge::create(array(
-	  "amount" => 1000,
-	  "currency" => "usd",
-	  "description" => "Example charge",
-	  "source" => $token,
-	));
+// echo $_POST["stripeToken"]
+		// Charge the user's card:
+		// ate a Customer:
+// $customer = \Stripe\Customer::create(array(
+//   "email" => "paying.user@example.com",
+//   "source" => $token,
+// ));
+// // Charge the Customer instead of the card:
+// $charge = \Stripe\Charge::create(array(
+//   "amount" => 1000,
+//   "currency" => "usd",
+//   "customer" => $customer->id
+// ));
+
+$charge = \Stripe\Charge::create(array(
+  "amount" => 1000,
+  "currency" => "usd",
+  "description" => "Example charge",
+  "source" => $token,
+));
+
 ?>
 
-Your token is <?php echo $_GET["token"]; ?>
 
 
 </body>
